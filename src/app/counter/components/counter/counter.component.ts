5bb5ce2e20b4eec12d0f53fdd5962cba173e052a
Felix <ftnuenz@asf.gob.mx>
2023-05-10 10:19:57 -0600

    first commit

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,26 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-counter',
+  template:`
+  <h3>Counter: {{ counter }}</h3>
+  <button (click)="increment()">+1</button>
+  <button (click)="reset()">Reset</button>
+  <button (click)="decrement()">-1</button>
+  `
+})
+export class CounterComponent{
+  public counter = 100;
+
+  increment(): void {
+    this.counter++;
+  }
+
+  decrement(): void {
+    this.counter--;
+  }
+
+  reset(): void {
+    this.counter = 100;
+  }
+}
---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+  public name: string = 'ironman'
+  public age: number = 20;
+
+  get capitalize(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero(): void {
+    this.name = 'Spiderman';
+  }
+
+  changeAge(): void {
+    this.age = 45;
+  }
+
+  resetForm(): void {
+    this.name = 'ironman';
+    this.age = 20;
+  }
+}
